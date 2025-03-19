4be08a2cb7cb0bbd165517fd4ee95c66b1b95451
Souradip <dgsouro77@gmail.com>
2025-03-19 22:32:11 +0530

    Added comments

--- Arrays/SmallestElement.java
+++ Arrays/SmallestElement.java
@@ -1,23 +1,30 @@
 package Arrays;
 
 import java.util.Scanner;
+
 public class SmallestElement {
     public static void main(String args[]) {
-        Scanner sc=new Scanner(System.in);
-        String small=null;
+        Scanner sc = new Scanner(System.in);
+        String small = null; // Variable to store the smallest string
+        
+        System.out.println("Enter the elements (Type 'RUKOOO' to stop):");
 
-        System.out.println("Enter the elements:");
-
-        while(sc.hasNext()) {
-            String str=sc.next();
-            if(str.equalsIgnoreCase("RUKOOO")) 
-            break;
-            if(small==null || str.compareTo(small)<0) {
-                small=str;
+        while (sc.hasNext()) {
+            String str = sc.next(); // Read the next string input
+            
+            // If the input is "RUKOOO" (case-insensitive), break the loop
+            if (str.equalsIgnoreCase("RUKOOO")) 
+                break;
+            
+            // Update 'small' if it's null or the new string is lexicographically smaller
+            if (small == null || str.compareTo(small) < 0) {
+                small = str;
             }
         }
-        sc.close();
-        System.out.println("Smallest element is :"+small);
+        
+        sc.close(); // Close the scanner to prevent resource leaks
+        
+        // Display the smallest string found
+        System.out.println("Smallest element is: " + small);
     }
 }
-   
