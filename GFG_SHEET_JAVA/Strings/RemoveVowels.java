46d68e3f8035ebb030b230e0ee3f30ea37df44fb
Souradip <dgsouro77@gmail.com>
2025-03-24 21:46:52 +0530

    All mighty push

--- GFG_SHEET_JAVA/Strings/RemoveVowels.java
+++ GFG_SHEET_JAVA/Strings/RemoveVowels.java
@@ -1,21 +1,31 @@
-package GFG_SHEET_JAVA.Strings;
+package GFG_SHEET_JAVA.Strings;  
 
-import java.util.Scanner;
+import java.util.Scanner;  
 
-public class RemoveVowels {
-        public static String remove(String s) {
-            StringBuilder res=new StringBuilder();
-            for(char ch:s.toCharArray()) {
-                if("aeiouAEIOU".indexOf(ch) == -1) {
-                    res.append(ch);
-                }
-            }
-            return res.toString();
-            
-        }
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-        String S = sc.next();
-        System.out.println(remove(S));
-        }
+public class RemoveVowels {     
+
+    // Method to remove vowels from the given string
+    public static String remove(String s) {         
+        StringBuilder res = new StringBuilder(); // Using StringBuilder for efficient string modification
+        
+        // Iterate through each character in the string
+        for (char ch : s.toCharArray()) {             
+            // Check if the character is not a vowel
+            if ("aeiouAEIOU".indexOf(ch) == -1) {                 
+                res.append(ch); // Append non-vowel characters to result
+            }         
+        }         
+        return res.toString(); // Return the modified string without vowels
+    }     
+
+    public static void main(String[] args) {         
+        Scanner sc = new Scanner(System.in); // Scanner object to take input from the user
+        
+        System.out.print("Enter a string: ");
+        String S = sc.next(); // Read user input
+        
+        System.out.println("String after removing vowels: " + remove(S)); // Display result
+        
+        sc.close(); // Close scanner to prevent resource leak
+    } 
 }
